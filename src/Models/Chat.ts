669a65bd54d31120630b3a9934db7b4230730f20
gjb2054 <gjb2054@rit.edu>
2022-10-31 14:00:09 -0400

    Updated Chat Model

---
+++ src/Models/Chat.ts
@@ -0,0 +1,59 @@
+import { Schema, model } from "mongoose";
+
+export interface Message {
+  content: String;
+  employerSent: boolean;
+  timeSent: Date;
+}
+
+const MessageSchema: Schema = new Schema<Message>({
+  content: {
+    type: String,
+    required: true,
+  },
+  employerSent: {
+    type: Boolean,
+    required: true,
+  },
+  timeSent: {
+    type: Date,
+    required: true,
+  },
+});
+
+interface Chat {
+  chatID: String;
+  match: String;
+  messages: Message[];
+  employerSilence: boolean;
+  candidateSilence: boolean;
+}
+
+const ChatSchema: Schema = new Schema<Chat>({
+  chatID: {
+    type: String,
+    required: true,
+  },
+  match: {
+    type: Schema.Types.ObjectId,
+    ref: "Match",
+    required: true,
+  },
+  messages: [
+    {
+      type: MessageSchema,
+      required: true,
+    },
+  ],
+  employerSilence: {
+    type: Boolean,
+    required: true,
+  },
+  candidateSilence: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const ChatModel = model("Chat", ChatSchema);
+export default ChatModel;
