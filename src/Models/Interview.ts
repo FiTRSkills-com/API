ba962004b8e52f025a40cb1abff9a17ba9afd36a
gjb2054 <gjb2054@rit.edu>
2022-10-31 14:03:05 -0400

    Updated Interview Model

--- src/Models/Interview.ts
+++ src/Models/Interview.ts
@@ -1,38 +1,16 @@
 import { Schema, model } from "mongoose";
 
 /**
- * @typedef {Object} Interviewer
- * @property {string} name Interviewer's name
- * @property {string} position Interviewer's position
- */
-interface Interviewer {
-  name: string;
-  position: string;
-}
-
-/**
- * @typedef {Object} InterviewDetails
- * @property {string} companyID The company's unique ID
- * @property {Interviewer} interviewer The interviewer's name and position
- * @property {string} notes Any notes from the interviewer
- */
-interface InterviewDetails {
-  company: string;
-  interviewer: string;
-  sid: string;
-  notes: string;
-}
-
-/**
  * @typedef {Object} Interview
  * @property {string} applicationID The application's unique ID
  * @property {Date} interviewDate The datetime of the interview
  * @property {InterviewDetails} interviewDetails The details of the interview
  */
 export interface Interview {
-  application: string;
+  interviewID: string;
+  match: string;
   interviewDate: Date;
-  interviewDetails: InterviewDetails;
+  twilloMeetingID: string;
 }
 
 /**
@@ -42,34 +20,21 @@ export interface Interview {
  * @property {Object} interviewDetails The interview's details (Optional)
  */
 const InterviewSchema: Schema = new Schema<Interview>({
-  application: {
+  interviewID: {
+    type: String,
+    required: true,
+  },
+  match: {
     type: Schema.Types.ObjectId,
-    ref: "Application",
     required: true,
   },
   interviewDate: {
     type: Date,
     required: true,
   },
-  interviewDetails: {
-    company: {
-      type: Schema.Types.ObjectId,
-      ref: "Company",
-      required: true,
-    },
-    interviewer: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: false,
-    },
-    sid: {
-      type: String,
-      required: false,
-    },
-    notes: {
-      type: String,
-      required: false,
-    },
+  twilloMeetingID: {
+    type: String,
+    required: true,
   },
 });
 
