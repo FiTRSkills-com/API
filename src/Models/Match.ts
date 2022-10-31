2bb7d37750b171f9d2d07f059b9cf552928b46cf
gjb2054 <gjb2054@rit.edu>
2022-10-31 13:43:39 -0400

    Updated Match and Status Model

---
+++ src/Models/Match.ts
@@ -0,0 +1,66 @@
+import { Schema, model } from "mongoose";
+
+/**
+ * @typedef {Object} Application
+ * @property {string} job Job posting's unique ID
+ * @property {string} user User's unique ID
+ * @property {string} status Status of Application
+ * @property {Timeslot[]} interviewTimeSlots Array of potential timeslots
+ */
+export interface Match {
+  matchID: string;
+  job: string;
+  candidate: string;
+  generalStatus: string;
+  candidateStatus: string;
+  employerStatus: string;
+  interview: string;
+}
+
+/**
+ * @typedef {Object} ApplicationSchema<Application>
+ * @property {string} job Job's unique ID (Required)
+ * @property {string} user User's unique ID (Required)
+ * @property {string} status Status of application
+ * @property {Object[]} interviewTimeSlots Array of interview time slots
+ */
+const MatchSchema: Schema = new Schema<Match>({
+  matchID: {
+    type: String,
+    required: true,
+  },
+  job: {
+    type: Schema.Types.ObjectId,
+    ref: "Job",
+    required: true,
+  },
+  candidate: {
+    type: Schema.Types.ObjectId,
+    ref: "Candidate",
+    required: true,
+  },
+  generalStatus: {
+    type: Schema.Types.ObjectId,
+    ref: "Status",
+    required: true,
+  },
+  candidateStatus: {
+    type: Schema.Types.ObjectId,
+    ref: "Status",
+    required: true,
+  },
+  employerStatus: {
+    type: Schema.Types.ObjectId,
+    ref: "Status",
+    required: true,
+  },
+  interview: {
+    type: Schema.Types.ObjectId,
+    ref: "Interview",
+    required: false,
+  },
+});
+
+// Create and export the model
+const MatchModel = model("Match", MatchSchema);
+export default MatchModel;
---
+++ src/Models/Status.ts
@@ -0,0 +1,50 @@
+import { Schema, model } from "mongoose";
+
+export enum generalStatus {
+  PRE_MATCH = "Pre Match",
+  MATCH = "Match",
+  RETRACTED = "Retracted",
+  INTERVIEW_SCHEDULED = "Inteview Scheduled",
+  POST_INTERVIEW = "Post Interview",
+}
+
+export enum candidateStatus {
+  CANDIDATE_INTERESTED = "Candidate Interested",
+  CANDIDATE_UNINTERESTED = "Candidate Uninterested",
+  CANDIDATE_RETRACTED = "Candidate Retracted",
+}
+
+export enum employerStatus {
+  EMPLOYER_INTERESTED = "Employer Interested",
+  EMPLOYER_UNINTERESTED = "Employer Uninterested",
+  EMPLOYER_RETRACTED = "Employer Retracted",
+}
+
+interface Status {
+  statusID: String;
+  currentStatus: String;
+  dateModified: Date;
+  previousStatus: String;
+}
+
+const StatusSchema: Schema = new Schema<Status>({
+  statusID: {
+    type: String,
+    required: true,
+  },
+  currentStatus: {
+    type: String,
+    required: true,
+  },
+  dateModified: {
+    type: Date,
+    required: true,
+  },
+  previousStatus: {
+    type: Schema.Types.ObjectId,
+    ref: "Status",
+  },
+});
+
+const StatusModel = model("Status", StatusSchema);
+export default StatusModel;
